b436b526d47ccf6121eff86959b06b4bd6073a28
moadmakboul <moad.makboul.mm@gmail.com>
2023-02-27 21:22:08 +0100

    side bar modifications

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request
 from flaskblog.models import Post
 from flask import Blueprint
 from flaskblog.users.utils import get_likes, get_dislikes
-from flaskblog import db
+
 
 main = Blueprint('main', __name__)
 
@@ -12,9 +12,10 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    latest_post = Post.query.order_by(Post.date_posted.desc()).first()
     likes = get_likes()
     dislikes = get_dislikes()
-    return render_template('home.html', title='Home Page', posts=posts, likes=likes, dislikes=dislikes)
+    return render_template('home.html', title='Home Page', posts=posts, likes=likes, dislikes=dislikes, latest_post=latest_post.title)
 
 
 @main.route('/about')
